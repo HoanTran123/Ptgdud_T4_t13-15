2bae0a4af1eaaf4d450b5905c9cf087b32cc6ab3
HoanTran123 <hoanvg78@gmail.com>
2025-01-08 20:27:04 +0700

    JSF Part1 Question 3

--- Demo/Tuan01/Cau3/cau3.js
+++ Demo/Tuan01/Cau3/cau3.js
@@ -15,4 +15,37 @@ if(avgDolphins > avgKoalas){
 }else{
     console.log("điểm trung bình của Dolphins thấp hơn Koalas")
 }
-//data 2
+//data bonus 1
+var Dolphins = [97,112,101]
+console.log("Các điểm của Dolphins : " +Dolphins)
+var Koalas =[109,95,123]
+console.log("Các điểm của Koalas : " +Koalas)
+
+var avgDolphins = (Dolphins[0] + Dolphins[1] + Dolphins[2])/3
+console.log("Điểm trung bình của Dolphins : " +avgDolphins)
+var avgKoalas = (Koalas[0] + Koalas[1] + Koalas[2])/3
+console.log("Điểm trung bình của Koalas : " +avgKoalas)
+if(avgDolphins > avgKoalas){
+    console.log("điểm trung bình của Dolphins cao hơn Koalas")
+}else if(avgDolphins == avgKoalas){
+    console.log("điểm trung bình của Dolphins bằng Koalas")
+}else{
+    console.log("điểm trung bình của Dolphins thấp hơn Koalas")
+}
+//Bonus 2
+var Dolphins = [97,112,101]
+console.log("Các điểm của Dolphins : " +Dolphins)
+var Koalas =[109,95,106]
+console.log("Các điểm của Koalas : " +Koalas)
+
+var avgDolphins = (Dolphins[0] + Dolphins[1] + Dolphins[2])/3
+console.log("Điểm trung bình của Dolphins : " +avgDolphins)
+var avgKoalas = (Koalas[0] + Koalas[1] + Koalas[2])/3
+console.log("Điểm trung bình của Koalas : " +avgKoalas)
+if(avgDolphins > avgKoalas){
+    console.log("điểm trung bình của Dolphins cao hơn Koalas")
+}else if(avgDolphins == avgKoalas){
+    console.log("điểm trung bình của Dolphins bằng Koalas")
+}else{
+    console.log("điểm trung bình của Dolphins thấp hơn Koalas")
+}
