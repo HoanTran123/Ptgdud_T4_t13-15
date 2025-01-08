70d55b42918d1389c702641ebe0a932ce7d21208
HoanTran123 <hoanvg78@gmail.com>
2025-01-08 20:14:31 +0700

    JSF Part1 Question1

---
+++ Demo/Tuan01/Cau2/cau2.js
@@ -0,0 +1,20 @@
+var massMark = 78;
+var heightMark = 1.69;
+var massJoin = 95;
+var heightJoin = 1.88;
+
+var BMIMark = (massMark/(heightMark*heightMark) );
+console.log("chỉ số BMI của Mark là : "+BMIMark)
+var BMIJoin = (massJoin/(heightJoin*heightJoin));
+console.log("chỉ số BMI của Join là : " +BMIJoin)
+
+if(BMIMark > BMIJoin){
+    console.log("Mark có  BMI cao hơn Join ")
+}else{
+    console.log("Join có  BMI cao hơn Mark ")
+}
+if(BMIMark > BMIJoin){
+    console.log("BMI của Mark :" + BMIMark + "  cao hơn của Join :" + BMIJoin)
+}else{
+    console.log("Join có  BMI cao hơn Mark ")
+}
---
+++ Demo/Tuan01/Cau3/cau3.js
@@ -0,0 +1,18 @@
+//data 1
+var Dolphins = [96,108,89]
+console.log("Các điểm của Dolphins : " +Dolphins)
+var Koalas =[88,91,100]
+console.log("Các điểm của Koalas : " +Koalas)
+
+var avgDolphins = (Dolphins[0] + Dolphins[1] + Dolphins[2])/3
+console.log("Điểm trung bình của Dolphins : " +avgDolphins)
+var avgKoalas = (Koalas[0] + Koalas[1] + Koalas[2])/3
+console.log("Điểm trung bình của Koalas : " +avgKoalas)
+if(avgDolphins > avgKoalas){
+    console.log("điểm trung bình của Dolphins cao hơn Koalas")
+}else if(avgDolphins == avgKoalas){
+    console.log("điểm trung bình của Dolphins bằng Koalas")
+}else{
+    console.log("điểm trung bình của Dolphins thấp hơn Koalas")
+}
+//data 2
